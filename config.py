14877a4d4fb9c128f28934525fe7f875ce39ca0d
lrraymond13 <lrraymond13@gmail.com>
2014-11-14 12:42:02 -0500

    adding Zulip API

--- config.py
+++ config.py
@@ -7,3 +7,8 @@ MMF_API_KEY = '24ceezruqtcz9fs7unkbah5vfzetm77h'
 MMF_SECRET = 'uD73HJercuvBb5DubAupebZS7mDJM3ZhNAb6P8e3hye'
 MMF_BEARER_ACCESS = 'Bearer 3594c7682d0023f7c915fa767d0491f1ee6f70d5'
 MMF_URL = 'https://oauth2-api.mapmyapi.com/v7.0/route/?close_to_location='
+
+#Zulip Running Bot Info
+ZULIP_BOT = 'RunningBot'
+ZULIP_USERNAME = 'RunningBot-bot@students.hackerschool.com'
+ZULIP_API_KEY = '6xclFawNnaFhdxNM3DBOPGcYyCD98zb7' 
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from Google_API import GoogleRequests
+from MMF_API import MMFRouteAPI
 
 
 if __name__ == '__main__':
@@ -6,3 +7,8 @@ if __name__ == '__main__':
 	lat, lon = get_coords.get_geocode('47 Delancey Street New York NY 10002')
 	print(lat)
 	print(lon)
+
+	new_req = MMFRouteAPI()
+	json_data = new_req.get_routes(lat, lon, 10, 0)
+	for l in new_req.list_runs(json_data):
+		print(l)
